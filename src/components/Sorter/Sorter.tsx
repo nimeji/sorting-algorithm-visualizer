d20bab0d6e6fba161b5696613ee14d60a02d5edf
Nimeji <yimeji95@gmail.com>
2021-07-25 13:05:09 +0200

    added compared values highlighting

--- src/components/Sorter/Sorter.tsx
+++ src/components/Sorter/Sorter.tsx
@@ -23,14 +23,13 @@ function* bubbleSort(array: Array<number>, swap: (i: number, j: number) => void)
   for(let i = 0; i < array.length - 1; i++) {
     let swapped = false;
     for(let j = 0; j < array.length - 1 - i; j++) {
-      console.log(i, j, array[j], array[j+1], array.length);
+      [array, swap] = yield [j, j+1];
       if(array[j] > array[j+1]) {
         swap(j, j+1);
 
         swapped = true;
         console.log('swap', array);
       }
-      [array, swap] = yield [j, j+1];
     }
     if(!swapped) break;
   }
@@ -42,7 +41,7 @@ export function Sorter({data}: SorterProps) {
 
   const [values, setValues] = useState<Array<number>>([]);
   const [generator, setGenerator] = useState<Generator>();
-  const [compared, setCompared] = useState<[number, number]>([-1, -1]);
+  const [compared, setCompared] = useState<[number | undefined, number | undefined]>([undefined, undefined]);
 
   const swap = useCallback((i: number, j: number) => {
     const temp = cloneArray(values);
@@ -64,7 +63,12 @@ export function Sorter({data}: SorterProps) {
 
   const elements = [];
   for(let i = 0; i < values.length; i++) {
-    elements[i] = <SorterValue key={i} height={values[i] * 100} width={100/values.length} />
+    elements[i] = <SorterValue 
+      key={i} 
+      height={values[i] * 100} 
+      width={100/values.length} 
+      selected={(compared[0] === i || compared[1] === i)}
+    />
   }
 
   return (
@@ -73,7 +77,7 @@ export function Sorter({data}: SorterProps) {
         {elements}
       </div>
       <button onClick={()=>{
-        if(generator) generator.next([values, swap]);
+        if(generator) setCompared(generator.next([values, swap]).value);
       }}>Continue</button>
     </div>
   );
