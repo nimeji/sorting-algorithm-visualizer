6182946855275a5722680c393a49373ed6214b42
Nimeji <yimeji95@gmail.com>
2021-07-26 20:17:27 +0200

    added memoization to prevent rerenders

--- src/components/SorterValue/SorterValue.tsx
+++ src/components/SorterValue/SorterValue.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import styles from './SorterValue.module.scss';
 
 export type SorterValueProps = {
@@ -7,11 +8,11 @@ export type SorterValueProps = {
   sorted?: boolean;
 }
 
-export function SorterValue({height, width, selected=false, sorted=false}: SorterValueProps) {
+export const SorterValue =  React.memo(({height, width, selected=false, sorted=false}: SorterValueProps) => {
   return (
     <div 
       className={`${styles.SorterValue} ${selected ? styles.Selected : ''} ${sorted ? styles.Sorted : ''}`} 
       style={{height: `${height}%`, width: `${width}%`}} 
     />
   );
-}
+});
