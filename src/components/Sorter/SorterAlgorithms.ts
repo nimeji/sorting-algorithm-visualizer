42aa014a7e42b2f4ed5600ea6262770a9458f8eb
Nimeji <yimeji95@gmail.com>
2021-07-28 20:24:04 +0200

    added CocktailShakerSort

--- src/components/Sorter/SorterAlgorithms.ts
+++ src/components/Sorter/SorterAlgorithms.ts
@@ -27,59 +27,58 @@ function* bubbleSort(array: SorterArray): SorterAlgorithmGenerator {
   return;
 }
 
-// function* cocktailShakerSort(array: SorterArray): SorterAlgorithmGenerator {
-//   let sortedUpper: number[] = [];
-//   let sortedLower: number[] = [];
-//   const length = array.length;
+function* cocktailShakerSort(array: SorterArray): SorterAlgorithmGenerator {
+let sorted = new Set<number>();
+  const length = array.length;
 
-//   if(length <= 0) return;
+  if(length <= 0) return;
 
-//   let lower = 0;
-//   let upper = length - 1;
-//   let swapped = false;
+  let lower = 0;
+  let upper = length - 1;
+  let swapped = false;
 
-//   while(lower < upper) {
-//     swapped = false;
+  while(lower < upper) {
+    swapped = false;
 
-//     for(let i = lower; i < upper; i++) {
-//       yield [i, i+1, [...sortedLower, ...sortedUpper]];
-//       if(!array.compare(i, i+1)) {
-//         array.swap(i, i+1);
-//         swapped = true;
-//       }
-//     }
-//     if(!swapped) {
-//       break;
-//     }
+    for(let i = lower; i < upper; i++) {
+      yield [i, i+1, sorted];
+      if(!array.compare(i, i+1)) {
+        array.swap(i, i+1);
+        swapped = true;
+      }
+    }
+    if(!swapped) {
+      break;
+    }
 
-//     sortedUpper = [upper, ...sortedUpper];
-//     upper = upper - 1;
-//     swapped = false;
-//     for(let i = upper; i > lower; i--) {
-//       yield [i-1, i, [...sortedLower, ...sortedUpper]];
-//       if(!array.compare(i-1, i)) {
-//         array.swap(i-1, i);
-//         swapped = true;
-//       }
-//     }
-//     if(!swapped) {
-//       break;
-//     }
+    sorted.add(upper);
+    upper = upper - 1;
+    swapped = false;
+    for(let i = upper; i > lower; i--) {
+      yield [i-1, i, sorted];
+      if(!array.compare(i-1, i)) {
+        array.swap(i-1, i);
+        swapped = true;
+      }
+    }
+    if(!swapped) {
+      break;
+    }
 
-//     sortedLower = [...sortedLower, lower];
-//     lower = lower + 1;
-//   }
+    sorted.add(lower);
+    lower = lower + 1;
+  }
 
-//   if(!swapped) {
-//     for(let i = lower; i < upper; i++) {
-//       sortedLower = [...sortedLower, i];
-//     }
-//   }
+  if(!swapped) {
+    for(let i = lower; i <= upper; i++) {
+      sorted.add(i);
+    }
+  }
 
-//   yield [undefined, undefined, [...sortedLower, ...sortedUpper]]
+  yield [undefined, undefined, sorted];
 
-//   return
-// }
+  return
+}
 
 function* selectionSort(array: SorterArray): SorterAlgorithmGenerator {
   const sorted = new Set<number>();
@@ -133,7 +132,7 @@ function* insertionSort(array: SorterArray): SorterAlgorithmGenerator {
 
 export const algorithms = {
   BubbleSort: bubbleSort,
-  // CocktailShakerSort: cocktailShakerSort,
+  CocktailShakerSort: cocktailShakerSort,
   SelectionSort: selectionSort,
   InsertionSort: insertionSort,
 }
