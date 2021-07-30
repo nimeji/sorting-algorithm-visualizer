a2b19c855bd3a2f3cb4083aa16ca9ce964aa10a6
Nimeji <yimeji95@gmail.com>
2021-07-30 15:44:15 +0200

    updated tests

--- src/components/Sorter/Sorter.test.tsx
+++ src/components/Sorter/Sorter.test.tsx
@@ -4,8 +4,11 @@ import { Sorter } from './Sorter';
 import { SorterLogic } from './SorterLogic';
 
 /*
-  Testing this module would require mocking the canvas context - too much work for now
+  Testing this module with 100% coverage would require mocking the canvas context - too much work for now
 */
+
+jest.mock('./SorterLogic');
+const MockedSorterLogic = SorterLogic as jest.Mocked<typeof SorterLogic>
 
 describe('Sorter', () => {
   let wrapper: ShallowWrapper<any, any, Sorter>;
@@ -25,133 +28,115 @@ describe('Sorter', () => {
 
   it('renders the canvas', () => {
     expect(wrapper.find(Canvas)).toHaveLength(1);
-  })
+  });
 
   it('passes a draw function to the canvas', ()=>{
     expect(typeof wrapper.find(Canvas).props().draw).toBe('function');
   });
 
-  describe.skip('without mocked SorterLogic', () => {
-    it('renders correct number of SorterValues', () => {
-      // todo
-    });
-  
-    it('has correct width for each SorterValue', () => {
-      // todo
-    });
-  
-    it('has correct height for each SorterValue', () => {
-      // todo
-    });
-  
-    it('has correct height for each SorterValue after prop change', () => {
-      // todo
-    });
+  it.skip('renders correct number of SorterValues', () => {
+    // todo
   });
-  
-  describe('with mocked SorterLogic', () => {
-    let mockStart: jest.SpyInstance
-    let mockPause: jest.SpyInstance; 
 
-    beforeEach(() => {
-      // jest.spyOn(SorterLogic.prototype, 'getLastState').mockReturnValue({
-      //   values: [...data], 
-      //   indicesSorted: new Set(sorted), 
-      //   lastCompared: [...compared], 
-      //   comparisons, 
-      //   accesses
-      // });
-      // jest.spyOn(SorterLogic.prototype, 'getSleepTime').mockReturnValue(time);
-      // jest.spyOn(SorterLogic.prototype, 'getRealTime').mockReturnValue(time);
-      mockStart = jest.spyOn(SorterLogic.prototype, 'start').mockImplementation(()=>{});
-      mockPause = jest.spyOn(SorterLogic.prototype, 'pause').mockImplementation(()=>{});
-    });
-
-    it.skip('shows the correct SorterValues as Sorted', () => {
-      // todo
-    });
-  
-    it.skip('shows the correct SorterValues as selected', () => {
-      // todo
-    });
-  
-    it.skip('shows comparisons correctly', () => {
-      // todo
-    });
-  
-    it.skip('shows accesses correctly', () => {
-      // todo
-    });
-  
-    it.skip('shows sleepTime correctly', () => {
-      // todo
-    });
-  
-    it.skip('shows realTime correctly', () => {
-      // todo
-    });
-
-    it('starts sorting when calling start', () => {
-      instance.start();
-      expect(mockStart).toHaveBeenCalled();
-    });
-
-    it('pauses sorting when calling pause', () => {
-      instance.pause();
-      expect(mockPause).toHaveBeenCalled();
-    });
-
-    it('continues sorting after pause', () => {
-      instance.start();
-      instance.pause();
-      expect(mockStart).toHaveBeenCalledTimes(1);
-      instance.start();
-      expect(mockStart).toHaveBeenCalledTimes(2);
-    });
-
-    it('indicates correctly whether the algorithm is running', () => {
-      const mockIsRunning = jest.spyOn(SorterLogic.prototype, 'isRunning').mockReturnValue(true);
-      expect(instance.isRunning()).toBeTruthy();
-      mockIsRunning.mockReturnValue(false);
-      expect(instance.isRunning()).toBeFalsy();
-    });
-
-    it('indicates correctly whether the algorithm is finished', () => {
-      const mockIsFinished = jest.spyOn(SorterLogic.prototype, 'isFinished').mockReturnValue(true);
-      expect(instance.isFinished()).toBeTruthy();
-      mockIsFinished.mockReturnValue(false);
-      expect(instance.isFinished()).toBeFalsy();
-    });
-
-    it('resets after calling reset', () => {
-      const prevLogic = instance.state.logic;
-      wrapper.instance().reset();
-      expect(instance.state.logic).not.toBe(prevLogic);
-    });
-
-    it('resets when data or algorithm changes', () => {
-      const mockReset = jest.spyOn(instance, 'reset');
-      const algorithm = 'QuickSort';
-
-      expect(algorithm).not.toBe(Sorter.defaultProps.algorithm);
-      expect(mockReset).not.toHaveBeenCalled();
-      wrapper.setProps({
-        algorithm
-      });
-      expect(mockReset).toHaveBeenCalledTimes(1);
-      wrapper.setProps({
-        data: [...data],
-      });
-      expect(mockReset).toHaveBeenCalledTimes(2);
-    });
-
-    it('sets the delay when sleepTime changes', () => {
-      const mockSetDelay = jest.spyOn(SorterLogic.prototype, 'setDelay');
-      expect(mockSetDelay).not.toHaveBeenCalled();
-      wrapper.setProps({
-        sleepTime: 1234,
-      });
-      expect(mockSetDelay).toHaveBeenCalled();
-    });
+  it.skip('has correct width for each SorterValue', () => {
+    // todo
   });
+
+  it.skip('has correct height for each SorterValue', () => {
+    // todo
+  });
+
+  it.skip('has correct height for each SorterValue after prop change', () => {
+    // todo
+  });
+
+  it.skip('shows the correct SorterValues as Sorted', () => {
+    // todo
+  });
+
+  it.skip('shows the correct SorterValues as selected', () => {
+    // todo
+  });
+
+  it.skip('shows comparisons correctly', () => {
+    // todo
+  });
+
+  it.skip('shows accesses correctly', () => {
+    // todo
+  });
+
+  it.skip('shows sleepTime correctly', () => {
+    // todo
+  });
+
+  it.skip('shows realTime correctly', () => {
+    // todo
+  });
+
+  it('starts sorting when calling start', () => {
+    instance.start();
+    expect(MockedSorterLogic.prototype.start).toHaveBeenCalled();
+  });
+
+  it('pauses sorting when calling pause', () => {
+    instance.pause();
+    expect(MockedSorterLogic.prototype.pause).toHaveBeenCalled();
+  });
+
+  it('continues sorting after pause', () => {
+    instance.start();
+    instance.pause();
+    expect(MockedSorterLogic.prototype.start).toHaveBeenCalledTimes(1);
+    instance.start();
+    expect(MockedSorterLogic.prototype.start).toHaveBeenCalledTimes(2);
+  });
+
+  it('indicates correctly whether the algorithm is running', () => {
+    const mockIsRunning = MockedSorterLogic.prototype.isRunning as jest.Mock 
+    mockIsRunning.mockReturnValue(true);
+    expect(instance.isRunning()).toBeTruthy();
+    mockIsRunning.mockReturnValue(false);
+    expect(instance.isRunning()).toBeFalsy();
+  });
+
+  it('indicates correctly whether the algorithm is finished', () => {
+    const mockIsFinished = MockedSorterLogic.prototype.isFinished as jest.Mock
+
+    mockIsFinished.mockReturnValue(true);
+    expect(instance.isFinished()).toBeTruthy();
+    mockIsFinished.mockReturnValue(false);
+    expect(instance.isFinished()).toBeFalsy();
+  });
+
+  it('resets after calling reset', () => {
+    const prevLogic = instance.state.logic;
+    wrapper.instance().reset();
+    expect(instance.state.logic).not.toBe(prevLogic);
+  });
+
+  it('resets when data or algorithm changes', () => {
+    const mockReset = jest.spyOn(instance, 'reset');
+    const algorithm = 'QuickSort';
+
+    expect(algorithm).not.toBe(Sorter.defaultProps.algorithm);
+    expect(mockReset).not.toHaveBeenCalled();
+    wrapper.setProps({
+      algorithm
+    });
+    expect(mockReset).toHaveBeenCalledTimes(1);
+    wrapper.setProps({
+      data: [...data],
+    });
+    expect(mockReset).toHaveBeenCalledTimes(2);
+  });
+
+  it('sets the delay when sleepTime changes', () => {
+    expect(MockedSorterLogic.prototype.setDelay).not.toHaveBeenCalled();
+    wrapper.setProps({
+      sleepTime: 1234,
+    });
+    expect(MockedSorterLogic.prototype.setDelay).toHaveBeenCalled();
+  });
+
 });
