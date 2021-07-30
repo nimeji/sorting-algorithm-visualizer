4cde61e03baa395b7ed3d1a318ba5ab908a2f63e
Nimeji <yimeji95@gmail.com>
2021-07-30 13:59:32 +0200

    converted back to class component

--- src/components/Sorter/Sorter.test.tsx
+++ src/components/Sorter/Sorter.test.tsx
@@ -1,13 +1,26 @@
-import { shallow, ShallowWrapper } from "enzyme";
-import { Canvas } from "../Canvas/Canvas";
-import { Sorter } from "./Sorter";
+import { shallow, ShallowWrapper } from 'enzyme';
+import { Canvas } from '../Canvas/Canvas';
+import { Sorter } from './Sorter';
+import { SorterLogic } from './SorterLogic';
 
+/*
+  Testing this module would require mocking the canvas context - too much work for now
+*/
 
 describe('Sorter', () => {
+  let wrapper: ShallowWrapper<any, any, Sorter>;
+  let instance: Sorter;
   const data = [0.6, 0.5, 0.4, 0.1, 0.8, 0.2, 1, 0.9, 0.71, 0.3];
-  let wrapper: ShallowWrapper<typeof Sorter>;
-  beforeAll(() => {
+  // const length = 10;
+  // const sorted = [0, 1, 2, length - 1];
+  // const compared: [number, number] = [0, 3];
+  // const comparisons = 123;
+  // const accesses = 456;
+  // const time = 123.456;
+
+  beforeEach(() => {
     wrapper = shallow(<Sorter data={data} />);
+    instance = wrapper.instance();
   });
 
   it('renders the canvas', () => {
@@ -16,5 +29,129 @@ describe('Sorter', () => {
 
   it('passes a draw function to the canvas', ()=>{
     expect(typeof wrapper.find(Canvas).props().draw).toBe('function');
+  });
+
+  describe.skip('without mocked SorterLogic', () => {
+    it('renders correct number of SorterValues', () => {
+      // todo
+    });
+  
+    it('has correct width for each SorterValue', () => {
+      // todo
+    });
+  
+    it('has correct height for each SorterValue', () => {
+      // todo
+    });
+  
+    it('has correct height for each SorterValue after prop change', () => {
+      // todo
+    });
+  });
+  
+  describe('with mocked SorterLogic', () => {
+    let mockStart: jest.SpyInstance
+    let mockPause: jest.SpyInstance; 
+
+    beforeEach(() => {
+      // jest.spyOn(SorterLogic.prototype, 'getLastState').mockReturnValue({
+      //   values: [...data], 
+      //   indicesSorted: new Set(sorted), 
+      //   lastCompared: [...compared], 
+      //   comparisons, 
+      //   accesses
+      // });
+      // jest.spyOn(SorterLogic.prototype, 'getSleepTime').mockReturnValue(time);
+      // jest.spyOn(SorterLogic.prototype, 'getRealTime').mockReturnValue(time);
+      mockStart = jest.spyOn(SorterLogic.prototype, 'start').mockImplementation(()=>{});
+      mockPause = jest.spyOn(SorterLogic.prototype, 'pause').mockImplementation(()=>{});
+    });
+
+    it.skip('shows the correct SorterValues as Sorted', () => {
+      // todo
+    });
+  
+    it.skip('shows the correct SorterValues as selected', () => {
+      // todo
+    });
+  
+    it.skip('shows comparisons correctly', () => {
+      // todo
+    });
+  
+    it.skip('shows accesses correctly', () => {
+      // todo
+    });
+  
+    it.skip('shows sleepTime correctly', () => {
+      // todo
+    });
+  
+    it.skip('shows realTime correctly', () => {
+      // todo
+    });
+
+    it('starts sorting when calling start', () => {
+      instance.start();
+      expect(mockStart).toHaveBeenCalled();
+    });
+
+    it('pauses sorting when calling pause', () => {
+      instance.pause();
+      expect(mockPause).toHaveBeenCalled();
+    });
+
+    it('continues sorting after pause', () => {
+      instance.start();
+      instance.pause();
+      expect(mockStart).toHaveBeenCalledTimes(1);
+      instance.start();
+      expect(mockStart).toHaveBeenCalledTimes(2);
+    });
+
+    it('indicates correctly whether the algorithm is running', () => {
+      const mockIsRunning = jest.spyOn(SorterLogic.prototype, 'isRunning').mockReturnValue(true);
+      expect(instance.isRunning()).toBeTruthy();
+      mockIsRunning.mockReturnValue(false);
+      expect(instance.isRunning()).toBeFalsy();
+    });
+
+    it('indicates correctly whether the algorithm is finished', () => {
+      const mockIsFinished = jest.spyOn(SorterLogic.prototype, 'isFinished').mockReturnValue(true);
+      expect(instance.isFinished()).toBeTruthy();
+      mockIsFinished.mockReturnValue(false);
+      expect(instance.isFinished()).toBeFalsy();
+    });
+
+    it('resets after calling reset', () => {
+      const prevLogic = instance.state.logic;
+      wrapper.instance().reset();
+      expect(instance.state.logic).not.toBe(prevLogic);
+    });
+
+    it('resets when data or algorithm changes', () => {
+      const mockReset = jest.spyOn(instance, 'reset');
+      const algorithm = 'QuickSort';
+
+      expect(algorithm).not.toBe(Sorter.defaultProps.algorithm);
+      expect(mockReset).not.toHaveBeenCalled();
+      wrapper.setProps({
+        algorithm
+      });
+      expect(mockReset).toHaveBeenCalledTimes(1);
+      wrapper.setProps({
+        data: [...data],
+      });
+      expect(mockReset).toHaveBeenCalledTimes(2);
+    });
+
+    it('sets the delay when sleepTime changes', () => {
+      const mockSetDelay = jest.spyOn(SorterLogic.prototype, 'setDelay');
+      expect(mockSetDelay).not.toHaveBeenCalled();
+      wrapper.setProps({
+        sleepTime: 1234,
+      });
+      expect(mockSetDelay).toHaveBeenCalled();
+    });
   });
 });
--- src/components/Sorter/Sorter.tsx
+++ src/components/Sorter/Sorter.tsx
@@ -1,5 +1,4 @@
-import { useEffect, useState } from "react";
-import { useCallback } from "react";
+import { Component } from "react";
 import { Canvas } from "../Canvas/Canvas";
 import { AlgorithmNames } from "./SorterAlgorithms";
 import { SorterLogic } from "./SorterLogic";
@@ -7,48 +6,110 @@ import styles from './Sorter.module.scss';
 
 type SorterProps = {
   data: number[];
-  algorithm?: AlgorithmNames;
-  sleepTime?: number,
-  run?: boolean,
-  border?: number;
-  defaultColor?: string;
-  comparedColor?: string;
-  sortedColor?: string;
+  algorithm: AlgorithmNames;
+  sleepTime: number,
+  run: boolean,
+  border: number;
+  defaultColor: string;
+  comparedColor: string;
+  sortedColor: string;
+  onFinished?: () => void;
 };
 
-export function Sorter ({
-  data, 
-  algorithm = 'BubbleSort', 
-  sleepTime = 100, 
-  run = false,
-  border = 0.5,
-  defaultColor = 'lightblue',
-  comparedColor = 'darkblue',
-  sortedColor = 'green',
-}: SorterProps) {
-  const [logic, setLogic] = useState<SorterLogic>();
+type SorterState = {
+  logic: SorterLogic,
+};
 
-  useEffect(() => {
-    return () => {logic?.pause();}
-  }, [logic]);
+export class Sorter extends Component<SorterProps, SorterState> {
+  static defaultProps = { 
+    algorithm: 'BubbleSort', 
+    sleepTime: 100, 
+    run: false,
+    border: 1,
+    defaultColor: 'lightblue',
+    comparedColor: 'red',
+    sortedColor: 'green',
+  };
 
-  useEffect(() => {
-    setLogic(new SorterLogic(data, algorithm, sleepTime));
-  }, [data, algorithm, sleepTime]);
+  constructor(props: SorterProps) {
+    super(props);
 
-  useEffect(() => {
-    if(run) {
-      logic?.start();
-    } else {
-      logic?.pause();
+    this.state = {
+      logic: new SorterLogic(props.data, props.algorithm, props.sleepTime),
+    };
+
+    this.draw= this.draw.bind(this);
+  }
+
+  componentDidMount() {
+    if(this.props.run) this.start();
+  }
+
+  componentDidUpdate(prevProps: SorterProps, prevState: SorterState) {
+    const { data, algorithm, sleepTime, run } = this.props;
+    const { logic } = this.state;
+
+    if (prevState.logic !== logic) {
+      prevState.logic.pause();
+      if(run) this.start();
     }
-  }, [logic, run]);
 
-  const draw = useCallback((ctx: CanvasRenderingContext2D, _, frameTime: number) => {
+    if (prevProps.data !== data || prevProps.algorithm !== algorithm) {
+      this.reset();
+    }
+
+    if (prevProps.sleepTime !== sleepTime) {
+      logic.setDelay(sleepTime);
+    }
+    
+    if (prevProps.run !== run) {
+      if(run) this.start();
+      else this.pause();
+    }
+  }
+
+  componentWillUnmount() {
+    this.pause();
+  }
+
+  start() {
+    this.state.logic.start();
+  }
+
+  pause() {
+    this.state.logic.pause();
+  }
+
+  reset() {
+    const { data, algorithm, sleepTime } = this.props;
+
+    this.setState({
+      logic: new SorterLogic(data, algorithm, sleepTime),
+    });
+  }
+
+  isRunning() {
+    return this.state.logic.isRunning();
+  }
+
+  isFinished() {
+    return this.state.logic.isFinished();
+  }
+
+  draw (ctx: CanvasRenderingContext2D, frameTime:number, avgFrameTime: number) {
+    const {
+      border,
+      defaultColor,
+      sortedColor,
+      comparedColor,
+    } = this.props;
+
+    const { logic } = this.state;
+
     if(logic) {
       const width = ctx.canvas.width;
       const height = ctx.canvas.height;
-      const borderWidth = Math.ceil(Math.max(width, height) / 100 * border)
+      const borderWidth = Math.ceil(Math.max(width, height) / 100 * border * 0.5)
       const innerWidth = width - 2 * borderWidth;
       const innerHeight = height - 2 * borderWidth;
 
@@ -87,7 +148,7 @@ export function Sorter ({
       ctx.fillText(`Sleep Time: ${(logic.getSleepTime()/1000).toFixed(1)}s`, textX, textY + fontSize * 4);
 
       if(!logic.isFinished()) {
-        ctx.fillText(`fps: ${(1000/frameTime).toFixed(0)}`, textX, textY + fontSize * 5);
+        ctx.fillText(`fps: ${(1000/avgFrameTime).toFixed(0)}`, textX, textY + fontSize * 5);
         return true;
       } else {
         ctx.fillText('fps: -', textX, textY + fontSize * 5);
@@ -96,11 +157,94 @@ export function Sorter ({
 
     }
     return false;
-  }, [logic, border, defaultColor, comparedColor, sortedColor]);
+  }
 
-  return (
-    <div className={styles.sorter}>
-      <Canvas draw={draw} />
-    </div>
-  );
+  render() {
+    return (
+      <div className={styles.sorter}>
+        <Canvas draw={this.draw} />
+      </div>
+    );
+  }
 }
+
+
+
+  // const [logic, setLogic] = useState<SorterLogic>();
+
+  // useEffect(() => {
+  //   return () => {logic?.pause();}
+  // }, [logic]);
+
+  // useEffect(() => {
+  //   setLogic(new SorterLogic(data, algorithm, sleepTime));
+  // }, [data, algorithm, sleepTime]);
+
+  // useEffect(() => {
+  //   if(run) {
+  //     logic?.start();
+  //   } else {
+  //     logic?.pause();
+  //   }
+  // }, [logic, run]);
+
+  // draw (ctx: CanvasRenderingContext2D, _, frameTime: number) => {
+  //   if(logic) {
+  //     const width = ctx.canvas.width;
+  //     const height = ctx.canvas.height;
+  //     const borderWidth = Math.ceil(Math.max(width, height) / 100 * border)
+  //     const innerWidth = width - 2 * borderWidth;
+  //     const innerHeight = height - 2 * borderWidth;
+
+  //     ctx.clearRect(0, 0, width, height);
+
+  //     const {values, lastCompared, indicesSorted, comparisons, accesses} = logic.getLastState();
+
+  //     const elementWidth = Math.floor(innerWidth / values.length);
+  //     const offset = (width - elementWidth * values.length) / 2;
+
+  //     values.forEach((value, i) => {
+  //       let color = defaultColor;
+
+  //       if(indicesSorted.has(i)) color = sortedColor;
+  //       if(lastCompared.includes(i)) color = comparedColor;
+
+  //       ctx.fillStyle = color;
+  //       ctx.fillRect(offset + elementWidth * i, innerHeight + borderWidth - innerHeight*value, elementWidth*1.05, innerHeight*value)
+  //     });
+
+  //     if(borderWidth > 0)
+  //     {
+  //       ctx.lineWidth = borderWidth;
+  //       ctx.strokeStyle = 'black';
+  //       ctx.strokeRect(offset - borderWidth * 0.5, borderWidth * 0.5, (width - 2 * offset + borderWidth), innerHeight + borderWidth);
+  //     }
+
+  //     const fontSize = innerHeight / 20;
+  //     const textX = offset + borderWidth
+  //     const textY = borderWidth;
+  //     ctx.font = `${fontSize}px sans-serif`;
+  //     ctx.fillStyle = 'black';
+  //     ctx.fillText(`Comparisons: ${comparisons}`,textX, textY  + fontSize);
+  //     ctx.fillText(`Array Accesses: ${accesses}`, textX, textY  + fontSize * 2);
+  //     ctx.fillText(`Real Time: ${logic.getRealTime().toFixed(1)}ms`, textX, textY + fontSize * 3);
+  //     ctx.fillText(`Sleep Time: ${(logic.getSleepTime()/1000).toFixed(1)}s`, textX, textY + fontSize * 4);
+
+  //     if(!logic.isFinished()) {
+  //       ctx.fillText(`fps: ${(1000/frameTime).toFixed(0)}`, textX, textY + fontSize * 5);
+  //       return true;
+  //     } else {
+  //       ctx.fillText('fps: -', textX, textY + fontSize * 5);
+  //       return false;
+  //     }
+
+  //   }
+  //   return false;
+  // }
+
+//   render() {
+//     <div className={styles.sorter}>
+//       <Canvas draw={this.draw} />
+//     </div>
+//   }
+// }
