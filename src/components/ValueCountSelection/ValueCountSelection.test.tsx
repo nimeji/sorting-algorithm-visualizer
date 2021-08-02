0ab79c079da03912eec537ae7f93564fe517f486
Nimeji <yimeji95@gmail.com>
2021-08-02 12:56:46 +0200

    added valueCount update on maxValue change

--- src/components/ValueCountSelection/ValueCountSelection.test.tsx
+++ src/components/ValueCountSelection/ValueCountSelection.test.tsx
@@ -1,4 +1,4 @@
-import { shallow, ShallowWrapper } from "enzyme";
+import { mount, ReactWrapper } from "enzyme";
 import React from "react";
 import { LabeledSelect } from "../LabeledSelect/LabeledSelect";
 import { ValueCountSelection } from "./ValueCountSelection";
@@ -12,12 +12,12 @@ for(let i = 16; i < maxValue; i *= 2) {
 values.push(maxValue);
 
 describe('ValueCountSelection', () => {
-  let wrapper: ShallowWrapper;
+  let wrapper: ReactWrapper;
   const callback = jest.fn();
   let onChange: React.ChangeEventHandler<HTMLSelectElement>;
 
   beforeEach(() => {
-    wrapper = shallow(<ValueCountSelection valueCount={32} maxValue={maxValue} onChange={callback} />)
+    wrapper = mount(<ValueCountSelection valueCount={32} maxValue={maxValue} onChange={callback} />)
     onChange = wrapper.find(LabeledSelect).props().onChange!;
   });
 
@@ -26,7 +26,7 @@ describe('ValueCountSelection', () => {
   });
 
   it('renders all options correctly', () => {
-    const options = wrapper.find(LabeledSelect).children();
+    const options = wrapper.find('option');
 
     options.forEach((option, i) => {
       expect(option.text()).toBe(values[i].toString());
@@ -54,5 +54,17 @@ describe('ValueCountSelection', () => {
     onChange({target: {value: 'abc'}} as React.ChangeEvent<HTMLSelectElement>);
 
     expect(callback).not.toHaveBeenCalled();
+  });
+
+  it('calls onChange callback correctly when maxValue decreases', () => {
+    wrapper.setProps({maxValue: 100, valueCount: 100});
+    wrapper.setProps({maxValue: 90});
+    expect(callback).toHaveBeenCalledWith(90);
+  });
+
+  it('calls onChange callback correctly when maxValue increases', () => {
+    wrapper.setProps({maxValue: 100, valueCount: 100});
+    wrapper.setProps({maxValue: 200});
+    expect(callback).toHaveBeenCalledWith(128);
   });
 });
--- src/components/ValueCountSelection/ValueCountSelection.tsx
+++ src/components/ValueCountSelection/ValueCountSelection.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useMemo } from "react";
 import { LabeledSelect } from "../LabeledSelect/LabeledSelect";
 
 type ValueCountSelectionProps = {
@@ -7,12 +8,30 @@ type ValueCountSelectionProps = {
 };
 
 export function ValueCountSelection({valueCount, maxValue, onChange}: ValueCountSelectionProps) {
-  const values: number[] = [];
-  
-  for(let i = 16; i < maxValue; i *= 2) {
-    values.push(i);
-  }
-  values.push(maxValue);
+  const values: number[] = useMemo(() => {
+    const result = [];
+
+    for(let i = 16; i < maxValue; i *= 2) {
+      result.push(i);
+    }
+    result.push(maxValue);
+
+    return result;
+  }, [maxValue]);
+
+  useEffect(() => {
+    if(!onChange) return;
+    
+    if(valueCount > maxValue) onChange(maxValue);
+    else if(valueCount < maxValue && !values.includes(valueCount)) {
+      for(let i = 0; i < values.length; i++) {
+        if(values[i] > valueCount) {
+          onChange(values[i]);
+          break;
+        }
+      }
+    }
+  }, [valueCount, maxValue, onChange, values]);
 
   return (
     <LabeledSelect text="Values" value={valueCount} onChange={(event) => {
