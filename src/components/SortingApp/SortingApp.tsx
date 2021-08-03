e17f21790b34bb290613d3566d70bb361c0c0cb7
Nimeji <yimeji95@gmail.com>
2021-08-03 20:54:11 +0200

    updated page layout

--- src/components/SortingApp/SortingApp.tsx
+++ src/components/SortingApp/SortingApp.tsx
@@ -1,12 +1,11 @@
-import { createRef, RefObject, useCallback, useMemo, useRef, useState } from 'react';
-import { Container, Nav, Navbar, Row } from 'react-bootstrap';
+import { useRef, useState } from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
 import { AlgorithmSelection } from '../AlgorithmSelection/AlgorithmSelection';
 import { DataPreset, DataPresetSelection } from '../DataPresetSelection/DataPresetSelection';
 import { SleepTimeSelection } from '../SleepTimeSelection/SleepTimeSelection';
 import { Sorter } from '../Sorter/Sorter';
-import { AlgorithmName, algorithmNames } from '../Sorter/SorterAlgorithms';
+import { AlgorithmName } from '../Sorter/SorterAlgorithms';
 import { ValueCountSelection } from '../ValueCountSelection/ValueCountSelection';
-import { WindowNumberSelect } from '../WindowNumberSelect/WindowNumberSelect';
 import styles from './SortingApp.module.scss';
 
 export function SortingApp() {
@@ -15,51 +14,14 @@ export function SortingApp() {
   const [valueCount, setValueCount] = useState(16);
   const [sleepTime, setSleepTime] = useState(500);
   const [maxValues, setMaxValues] = useState(valueCount);
-  const [numWindows, setNumWindows] = useState(1);
+  const [algorithm, setAlgorithm] = useState<AlgorithmName>('BubbleSort');
 
-  const [algorithms, setAlgorithms] = useState<AlgorithmName[]>([...algorithmNames]);
 
-  const setAlgorithm = useCallback((i: number, value: AlgorithmName) => {
-    setAlgorithms((previous) => {
-      const temp = [...previous];
-      temp[i] = value;
-
-      return temp;
-    })
-  }, [setAlgorithms]);
-
-  const sorterRef = useRef<RefObject<Sorter>[]>([]);
-
-  for(let i = 0; i<8; i++) {
-    sorterRef.current[i] = sorterRef.current[i] ?? createRef<Sorter>();
-  }
-
-  const windows = useMemo(() => {
-    const temp = [];
-    
-    for(let i = 0; i<numWindows; i++) {
-      temp[i] = (
-        <div key={i} className={`${styles.sorterContainer} col-xl-6`} style={{height: '400px'}}>
-          <div className={styles.algorithmSelection} >
-            <AlgorithmSelection algorithm={algorithms[i]} onChange={(algorithm) => setAlgorithm(i, algorithm)} />
-          </div>
-          <Sorter 
-            ref={sorterRef.current[i]} 
-            data={data} 
-            algorithm={algorithms[i]} 
-            sleepTime={sleepTime} 
-            onMaxValuesChange={setMaxValues} 
-          />
-        </div>
-      );
-    }
-
-    return temp;
-  }, [numWindows, algorithms, data, setAlgorithm, sleepTime]);
+  const sorterRef = useRef<Sorter>(null);
 
   return (
     <div className={styles.appContainer}>
-      <Navbar collapseOnSelect expand="lg" className="navbar-dark bg-secondary">
+      <Navbar collapseOnSelect expand="xl" className="navbar-dark bg-secondary">
         <Container fluid>
           <Navbar.Toggle />
           <Navbar.Brand>S-A-V</Navbar.Brand>
@@ -75,33 +37,38 @@ export function SortingApp() {
                 <SleepTimeSelection sleepTime={sleepTime} onChange={setSleepTime} options={[500, 250, 100, 50, 25, 10, 5, 1]} />
               </Nav.Item>
               <Nav.Item>
-                <WindowNumberSelect number={numWindows} onChange={setNumWindows} />
+                <AlgorithmSelection algorithm={algorithm} onChange={setAlgorithm} />
               </Nav.Item>
             </Nav>
           </Navbar.Collapse>
         </Container>
       </Navbar>
 
-      <Container>
-        <Row>
-          {windows}
-        </Row>
+      <Container fluid className={styles.sorterContainer}>
+        <Sorter 
+            ref={sorterRef} 
+            data={data} 
+            algorithm={algorithm} 
+            sleepTime={sleepTime} 
+            onMaxValuesChange={setMaxValues} 
+        />
       </Container>
-      <Nav className="fixed-bottom navbar-dark bg-secondary py-2">
+
+      <Nav className="navbar-dark bg-secondary py-2">
         <Container>
           <button 
             className="btn btn-success"
-            onClick={()=>sorterRef.current?.forEach(ref => ref.current?.start())}
+            onClick={()=>sorterRef.current?.start()}
           ><span className="bi bi-play-fill" /> Start</button>
 
           <button 
             className="btn btn-primary" 
-            onClick={()=>sorterRef.current?.forEach(ref => ref.current?.pause())}
+            onClick={()=>sorterRef.current?.pause()}
           ><span className="bi bi-pause-fill" /> Pause</button>
 
           <button 
             className="btn btn-danger"
-            onClick={()=>sorterRef.current?.forEach(ref => ref.current?.reset())}
+            onClick={()=>sorterRef.current?.reset()}
           ><span className="bi bi-arrow-counterclockwise" /> Reset</button>
         </Container>
       </Nav>
